048655c37f8fcea7fd3fb3bea221099de2cdbbd8
opilane <opilane@HP-CZC6297BBL>
2022-01-14 12:44:28 +0200

    .

--- face_.py
+++ face_.py
@@ -21,31 +21,31 @@ def lisa_eyes():
         c.create_oval((200, 200, 100, 100),  fill="thistle", outline="thistle") #левый глаз
 def lisa_nao():
     if var_nao.get()=="Nägu":
-        c.create_oval((10, 10, 490, 490), fill="peachpuff", outline="black") #лицо
+        c.create_oval((10, 10, 490, 490), fill="lavender", outline="black") #лицо
     elif var_nao.get()=="tühi":
         c.create_oval((10, 10, 490, 490), fill="thistle", outline="thistle") #лицо
 aken=Tk()
 aken.title("Face")
-aken.geometry('600x600')
+aken.geometry('800x600')
 aken.configure(bg="mediumslateblue")
 aken.grab_set()
-    
+lbl=Label(aken,text="<3",height=2,width=30,font="Arial 20",fg="blueviolet",bg="aliceblue",relief=GROOVE)    
 c = Canvas(aken, width=500, height=500, bg="thistle")
 var_nina=StringVar()
 ch_nina=Checkbutton(aken,text="Nina", variable=var_nina, onvalue="Nina", offvalue="tühi",bg="lightsteelblue",command=lisa_nina)
-ch_nina.pack()
+ch_nina.pack(side=RIGHT)
 var_suu=StringVar()
 ch_suu=Checkbutton(aken,text="Suu", variable=var_suu, onvalue="Suu", offvalue="tühi",bg="lightsteelblue",command=lisa_suu)
-ch_suu.pack()
+ch_suu.pack(side=RIGHT)
 var_eyes=StringVar()
 ch_eyes=Checkbutton(aken,text="Silmad", variable=var_eyes, onvalue="Silmad", offvalue="tühi",bg="lightsteelblue",command=lisa_eyes)
-ch_eyes.pack()
+ch_eyes.pack(side=RIGHT)
 var_nao=StringVar()
 ch_nao=Checkbutton(aken,text="Nägu", variable=var_nao, onvalue="Nägu", offvalue="tühi",bg="lightsteelblue",command=lisa_nao)
-ch_nao.pack()
+ch_nao.pack(side=RIGHT)
 
 c.pack(side=RIGHT)
-
+lbl.pack()
 aken.mainloop()
 
 
